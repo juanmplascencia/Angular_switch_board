f85d690229d3fb10138b62e12e56bbfaebd5889f
Juan <plascencia.juan831@gmail.com>
2017-10-12 17:12:07 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Switchboard';
+  switchboard: boolean[] = [false,false,false,false,false,false,false,false,false,false];
+  switchOnOff(index,data){
+    this.switchboard[index] = data;
+  }
 }
